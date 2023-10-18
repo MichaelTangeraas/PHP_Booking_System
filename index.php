8f84d4d2a6575f938ef3e9ac4867f6a9eaec2ea9
Hans Christian Morka <hanscm@uia.no>
2023-10-18 19:01:20 +0200

    Created small prototype for main booking page

--- index.php
+++ index.php
@@ -32,15 +32,6 @@
             font-size: 15px;
             text-align: center;
         }
-
-        .calenderValue {
-            border: 0;
-            outline: 0;
-        }
-
-        .calenderValue:focus {
-            outline: none !important;
-        }
     </style>
 </head>
 
@@ -49,76 +40,77 @@
         <input type="submit" name="booking" value="Book veiledning">
         <input type="text" name="text" placeholder="Booking info">
         <select name="day" form="bookingForm">
-    <option value="" disabled selected>Dag</option>
-  <option value="Mandag">Mandag</option>
-  <option value="Tirsdag">Tirsdag</option>
-  <option value="Onsdag">Onsdag</option>
-  <option value="Torsdag">Torsdag</option>
-  <option value="Fredag">Fredag</option>
-</select>
-<input type="number" name="time" min="8" max="17" placeholder="Tid">
+            <option value="" disabled selected>Dag</option>
+            <option value="Mandag">Mandag</option>
+            <option value="Tirsdag">Tirsdag</option>
+            <option value="Onsdag">Onsdag</option>
+            <option value="Torsdag">Torsdag</option>
+            <option value="Fredag">Fredag</option>
+        </select>
+        <input type="number" name="time" min="8" max="17" placeholder="Tid">
     </form>
-   
+
     <?php
     function newValue($timeDate)
     {
-        if (isset($_REQUEST['booking']) && $timeDate == $_REQUEST['day'].$_REQUEST['time']) {
+        if (isset($_REQUEST['booking']) && $timeDate == $_REQUEST['day'] . $_REQUEST['time']) {
             echo $_REQUEST['text'];
         } else {
             echo "Ledig";
         }
     }
 
-    function createTime(){
+    function createTime()
+    {
         $time = 8;
         echo '<div class="large-grid-item">
         <div class="grid-container">';
         $i = -1;
-        while ($i <=8){
+        while ($i <= 8) {
             $i++;
             echo '<div class="grid-item">';
-            echo $time++.":00";
+            echo $time++ . ":00";
             echo '</div>';
         }
         echo '</div></div>';
     }
 
-    function createDay($day){
+    function createDay($day)
+    {
         $time = 8;
-        $timeDate = $day.$time;
+        $timeDate = $day . $time;
         echo '<div class="large-grid-item">
         <div class="grid-container">';
         $i = -1;
-        while ($i <=8){
+        while ($i <= 8) {
             $i++;
-            $timeDate = $day.$time++;
+            $timeDate = $day . $time++;
             echo '<div class="grid-item">';
-        newValue($timeDate);
-        echo '</div>';
+            newValue($timeDate);
+            echo '</div>';
         }
         echo '</div></div>';
     }
     ?>
 
-        <div class="main-grid-container" style="border: 10px">
-            <div class="large-grid-item">Uke 42</div>
-            <div class="large-grid-item">Mandag 16.10</div>
-            <div class="large-grid-item">Tirsdag 17.10</div>
-            <div class="large-grid-item">Onsdag 18.10</div>
-            <div class="large-grid-item">Torsdag 19.10</div>
-            <div class="large-grid-item">Fredag 20.10</div>
+    <div class="main-grid-container" style="border: 10px">
+        <div class="large-grid-item">Uke 42</div>
+        <div class="large-grid-item">Mandag 16.10</div>
+        <div class="large-grid-item">Tirsdag 17.10</div>
+        <div class="large-grid-item">Onsdag 18.10</div>
+        <div class="large-grid-item">Torsdag 19.10</div>
+        <div class="large-grid-item">Fredag 20.10</div>
 
-            <?php
-            createTime();
-            createDay("Mandag");
-            createDay("Tirsdag");
-            createDay("Onsdag");
-            createDay("Torsdag");
-            createDay("Fredag");
-            ?>
-            
-            
-        </div>
+        <?php
+        createTime();
+        createDay("Mandag");
+        createDay("Tirsdag");
+        createDay("Onsdag");
+        createDay("Torsdag");
+        createDay("Fredag");
+        ?>
+
+    </div>
 </body>
 
 </html>
